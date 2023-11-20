e05a4773d1138401c1e8050b0e48df6f71d8efb0
WILLIAM ELLIS <will@wearestudio77.com>
2023-11-20 10:09:33 -0600

    added Schema

---
+++ models/shops.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose
+
+const shopSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    location:{
+        type:String,
+        required:true
+    }
+})
+
+const Shop = mongoose.model("Shop",shopSchema);
+module.exports = Shop
