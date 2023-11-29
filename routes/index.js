6c9031c81319f15179a5d5e604e992997e487b53
WILLIAM ELLIS <will@wearestudio77.com>
2023-11-29 10:54:48 -0600

    changed middleware name

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
     const API_ENDPOINT = "/api";
     const API_VERSION = "v1";
   
-    app.use(`${API_ENDPOINT}/${API_VERSION}/shops`, require("./shop"));
+    app.use(`${API_ENDPOINT}/${API_VERSION}/tattoo-shops`, require("./shop"));
   
     // catch all route to show 404 error whe all other roputes fail
     app.all("*", (req, res) => {
