aededf7ff77bb7535252645cac4d94a14373d42e
WILLIAM ELLIS <will@wearestudio77.com>
2023-11-29 11:15:12 -0600

    rejig of api folder

--- api/index.js
+++ api/index.js
@@ -1,8 +1,5 @@
 require("dotenv").config();
 const server = require("../server.js");
 
-const { PORT = 3000, NODE_ENV = "development" } = process.env;
+module.exports = server;
 
-server.listen(PORT, () => {
-  console.log(`Server running on: http://localhost:${PORT}`);
-});
